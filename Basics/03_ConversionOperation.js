7cfd53ff2f44ed79d031a2cc353d9dd2748a339e
Aman007-bit <kunwarpalkatheriya@gmail.com>
2025-09-19 06:40:32 +0000

    ConversionOperations

---
+++ Basics/03_ConversionOperation.js
@@ -0,0 +1,38 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score =  null
+let score = undefined
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// 33 => 33
+// "33" => 33
+// "33abc" => NaN [not a number]
+// null => 0
+// undefined => NaN
+// true => 1 and false => 0
+// "Aman" => NaN
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Aman"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof BooleanisLoggedIn);
+console.log(BooleanisLoggedIn);
+
+
+// 1 => true and 0 => false
+// "" => false
+// "Aman" = true
+
+
+let age = 33
+let StringisAge = String(age)
+console.log(typeof StringisAge);
+console.log(StringisAge)
+ 
