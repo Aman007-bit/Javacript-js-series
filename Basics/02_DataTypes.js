87ee62c12e8fba519cebc488d6106c23ec1a7e4f
Aman007-bit <kunwarpalkatheriya@gmail.com>
2025-09-19 03:35:38 +0000

    DataTypes

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+// this will treat all JS code as new version code.
+"use strict";  
+
+// alert(3 + 3)   it will throw error because we are using nodeJs not browser
+
+let name = "Aman"
+let age = 34
+let loggedIn = false
+
+// number => 2 to power 53
+// bingit  (we use this when number bigger)
+// string => "Aman"
+// boolean => true/false
+// null  (it is  a standalone value, and it is a object)
+// Undefined (rightnow value is not assign)
+// symbol (it is used to find uniqueness in figma etc.)
+
+
+// typeof is used know the type of data
+console.log(typeof "Aman");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
