7ac961ae19362ed6c88c64c51e8241bd133acec6
Aman007-bit <kunwarpalkatheriya@gmail.com>
2025-09-19 03:08:03 +0000

    Variables

---
+++ Basics/01_Var.js
@@ -0,0 +1,25 @@
+// These are the ways to store var in memory
+const accountId = 142356
+let accountCity = "jaipur"
+
+// we should avoid to declare variable using var 
+// because of issue in scope block and functional block
+var accounEmail  = "Aman@gmail.com"
+
+// when we do not know the values, only need to reserve memory for this var,
+// for future use and now its give us "Undefined Value".
+let accounState;
+
+// we can declare variable like this, we should avoid
+accountPassword = 12345 
+
+// accountId = 234  we cannot change constant varibale in js
+
+accounEmail = "sarvan@gmail.com"
+accountCity = "Bengaluru"
+accountPassword = 345
+
+// This are the ways to print var in js
+console.log(accounEmail);
+console.table([accountId, accountCity,accounEmail,accountPassword,accounState]);
+
