7bb5ca7e587dd24210f93234a0a3d81fdb933ea8
Aman007-bit <kunwarpalkatheriya@gmail.com>
2025-09-19 07:04:41 +0000

    ConversionOperations

--- Basics/03_ConversionOperation.js
+++ Basics/03_ConversionOperation.js
@@ -35,4 +35,39 @@ let age = 33
 let StringisAge = String(age)
 console.log(typeof StringisAge);
 console.log(StringisAge)
+
+// ******************** Operations ********************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+let str1 = "hello"
+let str2 =  " Aman"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) *  5 % 3);
+
+console.log(true);
+console.log(+true);
+// console.log(true+);  we cannot do this , it will give us error
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
  
